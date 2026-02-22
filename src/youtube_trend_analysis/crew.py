ac47833c9881d7d4b413dd99a5733a8ee7aca826
cafesuada <108068915+cafesuada24@users.noreply.github.com>
2026-02-22 17:07:18 +0700

    feat(agents): implement crew

---
+++ src/youtube_trend_analysis/crew.py
@@ -0,0 +1,49 @@
+from crewai import Agent, Crew, Process, Task
+from crewai.agents.agent_builder.base_agent import BaseAgent
+from crewai.project import CrewBase, agent, crew, task
+
+
+@CrewBase
+class YoutubeTrendAnalysisCrew:
+    agents_config = 'config/agents.yaml'
+    tasks_config = 'config/tasks.yaml'
+
+    agents: list[BaseAgent]
+    tasks: list[Task]
+
+    @agent
+    def analysis_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config['analysis_agent'],  # type: ignore[index]
+            verbose=True,
+            tools=[],
+        )
+
+    @agent
+    def response_synthesizer_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config['analysis_agent'],  # type: ignore[index]
+            verbose=True,
+        )
+
+    @task
+    def analysis_task(self) -> Task:
+        return Task(
+            config=self.agents_config['analysis_task'],  # type: ignore[index]
+        )
+
+    @task
+    def response_task(self) -> Task:
+        return Task(
+            config=self.agents_config['response_task'],  # type: ignore[index]
+        )
+
+    @crew
+    def crew(self) -> Crew:
+        """Create latest crew."""
+        return Crew(
+            agents=self.agents,
+            tasks=self.tasks,
+            process=Process.sequential,
+            verbose=True,
+        )
