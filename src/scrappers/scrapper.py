7c005fed1574cbb41b16b5bca2b6164f7b7acdb8
cafesuada <108068915+cafesuada24@users.noreply.github.com>
2026-02-22 23:23:05 +0700

    feat(scrapper): implement scrapper protocol

---
+++ src/scrappers/scrapper.py
@@ -0,0 +1,23 @@
+from collections.abc import Iterable
+from typing import Literal, Protocol
+
+
+class Scrapper(Protocol):
+    def scrape_channels(
+        self,
+        channel_urls: Iterable[str],
+        num_of_posts: int,
+        start_date: str,
+        end_date: str,
+        order_by: str,
+        country: str,
+    ) -> None | dict: ...
+
+    def get_progress(self, snapshot_id: str) -> None | dict: ...
+
+    def get_output(
+        self,
+        snapshot_id: str,
+        *,
+        output_format: Literal['json', 'jsonl'],
+    ) -> None | list[dict]: ...
